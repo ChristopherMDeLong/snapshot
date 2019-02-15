dca4fa4f5e5dfe345efa7087aded0985dd7ed833
Christopher DeLong <christopherdelong415@gmail.com>
2019-02-15 12:27:08 -0500

    yelp fetch

--- app/javascript/react/components/ReportInfoTile.js
+++ app/javascript/react/components/ReportInfoTile.js
@@ -1,18 +1,22 @@
 import React from "react";
 
 const ReportInfoTile = props => {
+
   return(
+
       <div className="rest-show">
-      <h5>Date: {props.date}</h5>
-      <h5>Food Sales: ${props.food_sales}</h5>
-      <h5>Liquor Sales: ${props.liquor_sales}</h5>
-      <h5>Total Sales: ${props.food_sales + props.liquor_sales}</h5>
-      <h5>Food Expenses: ${props.food_expenses}</h5>
-      <h5>Liquor Expenses: ${props.liquor_expenses}</h5>
-      <h5>Front of House Payroll: ${props.front_house_payroll}</h5>
-      <h5>Back of House Payroll: ${props.back_house_payroll}</h5>
-      <h5>Manager Payroll: ${props.manager_payroll}</h5>
-      <h5>HeadCount: {props.headcount}</h5>
+        <h5>Date: {props.date}</h5>
+        <h5>Food Sales: ${props.foodSales}</h5>
+        <h5>Liquor Sales: ${props.liquorSales}</h5>
+        <h5>Food Expenses: ${props.foodExpenses}</h5>
+        <h5>Liquor Expenses: ${props.liquorExpenses}</h5>
+        <h5>Front of House Payroll: ${props.frontHousePayroll}</h5>
+        <h5>Back of House Payroll: ${props.backHousePayroll}</h5>
+        <h5>Manager Payroll: ${props.managerPayroll}</h5>
+        <h5>Head Count: {props.headcount}</h5>
+        <h5>Total Sales: ${props.total}</h5>
+        <h5>Total Payroll: ${props.totalPayroll}</h5>
+        <h5>Total Expenses: ${props.totalExpenses}</h5>
       </div>
   );
 };
