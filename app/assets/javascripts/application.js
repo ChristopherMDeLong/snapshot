797b2cdf20a35ec97af4d3529783a8ee53815340
Christopher DeLong <christopherdelong415@gmail.com>
2019-02-16 17:10:48 -0500

    added totals chart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require chartkick
 //= require Chart.bundle
+//= require chartkick
 //= require activestorage
 //= require_tree .
 
