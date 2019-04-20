fc1c959f58471c2a24f587d6ce1ee6d0bcc8567c
Christopher DeLong <christopherdelong415@gmail.com>
2019-04-20 08:58:49 -0400

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
+  resources :about, only: [:index]
+  resources :our_services, only: [:index]
+  resources :rates, only: [:index]
+  resources :careers, only: [:index]
+  resources :media, only: [:index]
+  resources :faqs, only: [:index]
+  resources :contact_us, only: [:index]
+
   resources :restaurants do
     resources :reports, only: [:create, :new, :edit, :update, :destroy]
   end
